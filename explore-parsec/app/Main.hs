672ae653f8b78951903afd5f3ff8e83ac1378d69
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-06-04 23:45:44 +0200

    Add more parsers.

--- explore-parsec/app/Main.hs
+++ explore-parsec/app/Main.hs
@@ -36,6 +36,38 @@ csvParser = lineParser `endBy` newline <* eof
   where lineParser = cellParser `sepBy` (char ',')
         cellParser = many $ noneOf ",\n"
 
+--
+
+dogCatParser :: Parsec String st String
+dogCatParser = (string "dog") <|> (string "cat")
+
+--
+
+camelCatTryParser:: Parsec String st String
+camelCatTryParser = try (string "camel") <|> (string "cat")
+
+--
+
+type TNumber = Int
+
+data TOperator = TAdd
+               | TSubtract
+                 deriving (Eq, Ord, Show)
+
+data TExpression = TNode (TExpression) TOperator (TExpression)
+                 | TTerminal TNumber
+                   deriving (Show)
+
+numberParser :: Parsec String st TNumber
+numberParser = read <$> (many $ oneOf "0123456789")
+
+operatorParser :: Parsec String st TOperator
+operatorParser = chooseOp <$> (oneOf "+-")
+  where chooseOp '+' = TAdd
+        chooseOp '-' = TSubtract
+
+
+-- try also attoparsec
 
 -- decompose each value in the type to understand whats wrong
 
