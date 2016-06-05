7dd0f35761171c590f68ced0846a598308ceacea
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-06-05 15:18:07 +0200

    Add more examples from paper.

--- explore-parsec/app/ParsecFastCombLibPaper.hs
+++ explore-parsec/app/ParsecFastCombLibPaper.hs
@@ -5,13 +5,35 @@ module ParsecFastCombLib where
 -- module to implement the example in the paper Parsec : A fast combinator parser by Daan Leijen
 
 import Text.Parsec
+import qualified Text.Parsec.Combinator as C
+import Text.Parsec.String (Parser)
 
 test p = parse p ""
 
-simple :: Stream s m Char => ParsecT s u m Char 
+simple :: Parser Char
 simple = letter
 
-word :: Stream s m Char => ParsecT s u m String
+word :: Parser String
 word = many letter
 
+openCloseParser :: Parser Char
+openCloseParser = do { char '('
+                       ; char ')'
+                     }
 
+parens :: Parser ()
+parens = do { char '('
+             ; parens
+             ; char ')'
+             ; parens
+            }
+         <|> return ()
+
+
+-- (<|>) is predictive 
+testOr = String "(a)" <|> String "(b)"
+
+testOrWorks = do { char '('
+                 ; char "a" <|> char "b"
+                 ; char ")"
+                 }
